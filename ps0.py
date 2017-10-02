4373277514d9829ce9db692d2434f1f5483a76ef
Lucas <lucas-chau@hotmail.com>
2017-10-02 17:04:59 +0800

    ps0 and ps1a started
    Going to complete ps1

---
+++ ps0.py
@@ -0,0 +1,6 @@
+import math
+
+x = float(input("Enter a number x: "))
+y = float(input("Enter a number y: "))
+print("x**y = "+str(round(x**y)))
+print("log(x) = "+str(round(math.log(x,2))))
---
+++ ps1a.py
@@ -0,0 +1,3 @@
+annual_salary = int(input("Enter your annual salary: "))
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
+total_cost = int(input("Enter the cost of your dream home: "))
